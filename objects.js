88a8b78538f6916419499df458ff66b3e75c7760
Alex S <alex.sunga@fliptech.co>
2018-07-31 07:23:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ var playlist = {
   
 }
 
-function updatePlaylistn (playlist, artistName, songTitle) {
+function updatePlaylist (playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   
   return playlist
